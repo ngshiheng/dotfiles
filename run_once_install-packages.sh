de7cbbf27c3558e9c1bce84d2e442ab0a40ba886
Jerry Ng <ngshiheng@gmail.com>
2022-02-15 21:26:13 +0800

    add post run echo

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
     # Install ohmyzsh
-    rm -rf $HOME/.oh-my-zsh
+    rm -rf "$HOME"/.oh-my-zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
     # Install zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 
     # Install zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
     # Install p10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 
-    chezmoi apply -v
-    exec zsh
+    echo "Restart your shell run 'chezmoi apply' & 'exec zsh'"
 else
     echo 'Installation failed. Please install zsh first.'
 fi

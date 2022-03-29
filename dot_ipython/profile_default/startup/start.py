24b64b93adb64aea0c2b60709d6fc206f2c5f0a4
Jerry Ng <ngshiheng@gmail.com>
2022-03-29 15:06:42 +0800

    add ipython

---
+++ dot_ipython/profile_default/startup/start.py
@@ -0,0 +1,29 @@
+import datetime
+import heapq
+import time
+import types
+import typing
+from collections import Counter, defaultdict, deque
+
+DISPLAY_NAME = "Jerry"
+HIDDEN = {"types", "typing"}
+
+
+def get_imports() -> typing.Iterator[str]:
+    for module_name, module_type in globals().items():
+        if module_name.startswith("_"):
+            continue
+
+        if isinstance(module_type, types.ModuleType) or isinstance(module_type, type):
+            yield module_type.__name__
+
+
+print(f"👋 Hello, {DISPLAY_NAME}!")
+
+imports = [f"{imp!r}" for imp in get_imports() if imp not in HIDDEN]
+filtered_imports = ", ".join(imports)
+
+print(
+    f"🐍 Successfully imported these {len(imports)} modules: {filtered_imports}.\n"
+    "👨‍💻 Happy coding."
+)

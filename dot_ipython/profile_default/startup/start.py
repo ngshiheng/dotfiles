6bc5f8ed13598430207c27f19a8cf621708ca102
Jerry Ng <ngshiheng@gmail.com>
2022-11-15 07:39:28 +0800

    remove print shadowing & add timeit decorator

--- dot_ipython/profile_default/startup/start.py
+++ dot_ipython/profile_default/startup/start.py
@@ -1,15 +1,35 @@
-import datetime
 import heapq
-import pprint
 import time
 import types
 import typing
 from collections import Counter, defaultdict, deque
+from pprint import pprint
 
 HIDDEN = {types.__name__, typing.__name__}
 
+def timeit(method):
+    """Decorator use to measure function run time.
+
+    Example usage:
+
+    @timeit
+    def foo():
+        print("bar")
+    """
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' %(method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
 def get_imports() -> typing.Iterator[str]:
+    """Get imported modules name dynamically from import statements."""
     for module_name, module_type in globals().items():
         if module_name.startswith("_"):
             continue
@@ -22,13 +42,5 @@ imports = [f"{imp!r}" for imp in get_imports() if imp not in HIDDEN]
 filtered_imports = ", ".join(imports)
 
 print(
-    f"Successfully imported these {len(imports)} modules: {filtered_imports}.\n"
-    f"Shadowing `print` with `pprint`.\n"
+    f"Imported these {len(imports)} modules: {filtered_imports}.\n"
 )
-
-pp = pprint.PrettyPrinter(
-    indent=2,
-    sort_dicts=False,
-)
-
-print = pp.pprint

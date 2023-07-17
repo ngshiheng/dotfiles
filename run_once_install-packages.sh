d02e88112b69c4e6f16aa92cdc8039f06b7b4baa
Jerry Ng <ngshiheng@gmail.com>
2023-07-17 09:54:37 +0800

    add setup direnv

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -84,6 +84,8 @@ brew install asdf --quiet
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin add python
+asdf plugin-add direnv
+asdf direnv setup --shell zsh --version latest
 
 # Install direnv
 # https://direnv.net/docs/installation.html

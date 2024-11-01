d9745f8c27fe415f94ea3051728cd65e781df63c
Jerry Ng <ngshiheng@gmail.com>
2024-11-01 22:39:52 +0800

    add zoxide and migrate ydiff to git-delta

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -60,6 +60,14 @@ brew install bat --quiet
 # https://github.com/BurntSushi/ripgrep#installation
 brew install ripgrep --quiet
 
+# Install zoxide
+# https://github.com/ajeetdsouza/zoxide
+brew install zoxide --quiet
+
+# Install delta
+# https://github.com/dandavison/delta
+brew install git-delta --quiet
+
 # Install rich
 # https://github.com/Textualize/rich-cli#macos
 brew install rich --quiet
